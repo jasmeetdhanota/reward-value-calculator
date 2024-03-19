4e406a58fbef0068117ea086a2c9c4a62faf1fc5
Jasmeet Dhanota <jasmeetdhanota@jasmeets-air.lan>
2024-03-19 12:29:45 -0400

    Task 1 changes

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,42 @@
+public class RewardValue {
+
+
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor accepting cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashToMiles(cashValue);
+    }
+
+    // Constructor accepting miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesToCash(milesValue);
+    }
+
+    // Method to convert cash value to miles
+    private double cashToMiles(double cash) {
+        return cash / 0.0035;
+    }
+
+    // Method to convert miles value to cash
+    private double milesToCash(double miles) {
+        return miles * 0.0035;
+    }
+
+    // Getter method for cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Getter method for miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+
+
+
+}
